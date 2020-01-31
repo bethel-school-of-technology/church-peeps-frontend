3a2354568cece72a3534b1a67c3cc748c336727c
Elizabeth Munsinger <emunsinger@gmail.com>
2020-01-30 23:39:15 -0600

    screens folder

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Nav from './Nav';
-import Home from './Home';
-import Profile from './Profile';
-import Prayer from './Prayer';
-import Login from './login';
-import Signup from './Signup';
+import Home from './screens/Home';
+import Profile from './screens/Profile';
+import Prayer from './screens/Prayer';
+import Login from './screens/login';
+import Signup from './screens/Signup';
+
 
 function App() {
   return (
---
+++ src/screens/Profile.js
@@ -0,0 +1,16 @@
+import React, {PropTypes} from "react";
+import "../App";
+
+function Profile() {
+  return (
+    <div>
+      <h1>Profile for </h1>
+      </div>
+  );
+}
+
+// Profile.propTypes = {
+//   username: PropTypes.string.isRequired
+// };
+
+export default Profile;
