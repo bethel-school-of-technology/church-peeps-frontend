4ce26d399e4d8fe54ab702710341799c90f787be
Elizabeth Munsinger <emunsinger@gmail.com>
2020-02-17 11:17:13 -0600

    update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -25,14 +25,19 @@ class Profile extends Component {
     };
 
     componentDidMount() {
+        // this.fetchusers().then(response => {
+        //     this.setState({
+        //         users: response.users
+        //     });
+        // });
         // this.fetchusers();
         axios.get('/users')
             .then(response => {
-                if (response.data.length > 0) {
+                // if (response.data.length > 0) {
                     this.setState({
                         users: response.data.map(user => user.username),
                     })
-                }
+                // }
             })
             .catch((error) => {
                 console.log(error);
@@ -40,25 +45,33 @@ class Profile extends Component {
 
 
     }
+    
     render() {
         console.log(this.state.users);
         if (this.state.users.length === 0) {
             return <div>Failed to fetch users</div>;
         }
         const users = this.state.users.map(user => (
-            <div key={user.UserId}>
-                <h2>Your Church Peeps</h2>
-                <div>
+                        <div key={user.users}>
+                            <h2>Your Church Peeps</h2>
+                
+                <li>{user.firstName} {user.lastName}</li>
+                
+                {/* <div>
                     <div>{this.state.users.firstName}</div>
                     <div> {this.state.users.lastName} </div>
-                    </div>
-                <input type="file" accept="image/*;capture=camera"></input>
-                <img src=""></img>
+                    </div> */}
+                {/* <input type="file" accept="image/*;capture=camera"></input>
+                <img src=""></img> */}
                 
-                <em>{user.firstName} {user.lastName}</em>: {user.firstName} {user.lastName}
+              <em>{user.username}</em>: {user.firstName} {user.lastName}
             </div>
         ));
         return <div>{users}</div>
     }
 }
 export default Profile;
+
+{/* <ul>
+            {this.state.users.map(user => <li>{user.firstName} {user.lastName}</li>)}
+        </ul> */}
